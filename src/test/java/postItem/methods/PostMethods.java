1db791beae28833cde5c3df493b4ca1278d8590c
tolstikoval <61480444+tolstikoval@users.noreply.github.com>
2021-01-17 19:00:16 +0300

    добавление методов с постмана

--- src/test/java/postItem/methods/PostMethods.java
+++ src/test/java/postItem/methods/PostMethods.java
@@ -1,50 +1,69 @@
 package postItem.methods;
 
-import static io.restassured.RestAssured.*;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class PostMethods {
-  public static Response getPost(){
-    given().when().post();
-    Response response = RestAssured.get("https://jsonplaceholder.typicode.com/posts/15")
+  public static Response getThePost() {
+    Response response = RestAssured.given()
+            .contentType("application/json")
+            .log().all()
             .header("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3")
-            .header("Content-Type", "application/javascript; charset=UTF-8")
             .header("Cookie", "__cfduid=d251dc7412395e651ea7e6d6139bdaaf91610459685")
-            .body("")
-            .asString();
+            .when()
+            .get("https://jsonplaceholder.typicode.com/posts/15");
     return response;
   }
+
   public static Response getPosts() {
-    Response response = RestAssured.get("https://jsonplaceholder.typicode.com/posts")
+    Response response = RestAssured.given()
+            .contentType("application/json")
+            .log().all()
             .header("Cookie", "__cfduid=d251dc7412395e651ea7e6d6139bdaaf91610459685")
-            .body("")
-            .asString();
+            .when()
+            .get("https://jsonplaceholder.typicode.com/posts");
     return response;
   }
-  public static Response PostRequest() {
-    Response response = RestAssured.post("https://jsonplaceholder.typicode.com/posts")
-            .header("Content-Type", "application/json")
+
+  public static Response postNewPost() {
+    Response response = RestAssured.given()
+            .contentType("application/json")
+            .log().all()
             .header("Cookie", "__cfduid=d251dc7412395e651ea7e6d6139bdaaf91610459685")
-            .body("{\n\t\"method\": \"POST\",\n    \"title\": \"fooo 556dhdd\",\n    \"body\": \"barscdcd44jjhhjffd\",\n    \"userId\": 1\n}")
-            .asString();
+            .body("{\n" +
+                    "\t\"method\": \"POST\",\n   " +
+                    " \"title\": \"fooo 556dhdd\",\n  " +
+                    " \"body\": \"barscdcd44jjhhjffd\",\n  " +
+                    " \"userId\": 1\n}")
+            .when()
+            .post("https://jsonplaceholder.typicode.com/posts");
     return response;
   }
-  public static Response Put() {
-    Response response = RestAssured.put("https://jsonplaceholder.typicode.com/posts/1")
-            .header("Content-Type", "application/json; charset=UTF-8")
+
+  public static Response changeThePost() {
+    Response response = RestAssured.given()
+            .contentType("application/json; charset=UTF-8")
+            .log().all()
             .header("Cookie", "__cfduid=d251dc7412395e651ea7e6d6139bdaaf91610459685")
-            .body("{\n\t\"method\": \"PUT\",\n    \"id\": 1,\n    \"title\": \"foo\",\n    \"body\": \"bar\",\n    \"userId\": 1\n}")
-            .asString();
+            .body("{\n" +
+                    "\t\"method\": \"PUT\",\n" +
+                    "  \"id\": 1,\n  " +
+                    "  \"title\": \"foo\",\n  " +
+                    "  \"body\": \"bar\",\n  " +
+                    "  \"userId\": 1\n}")
+            .when()
+            .put("https://jsonplaceholder.typicode.com/posts/1");
     return response;
   }
-  public static Response deletePost() {
-    Response response = RestAssured.delete("https://jsonplaceholder.typicode.com/posts/15?accept=*/*")
-            .header("Content-Type", "application/json")
+
+  public static Response deleteThePost() {
+    Response response = RestAssured.given()
+            .contentType("application/json")
+            .log().all()
             .header("Cookie", "__cfduid=d251dc7412395e651ea7e6d6139bdaaf91610459685")
-            .body("{\n\t\"method\": \"DELETE\"\n}")
-            .asString();
+            .when()
+            .delete("https://jsonplaceholder.typicode.com/posts/15?accept=*/*");
     return response;
   }
 }
